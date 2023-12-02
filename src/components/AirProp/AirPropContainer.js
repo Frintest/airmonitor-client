9ac6b7cff27be85d512e4391a5a35678a35682c9
Dmitry <dimaruzaev2@gmai.com>
2023-12-02 18:16:10 +0300

    add common component with-router

--- src/components/AirProp/AirPropContainer.js
+++ src/components/AirProp/AirPropContainer.js
@@ -1,18 +1,10 @@
-import React from "react";
 import { AirProp } from "./AirProp.jsx";
 import { connect } from "react-redux";
 import { updateAirStateThunk } from "../../redux/screens-reducer";
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-
-const withRouter = (Component) => (props) => {
-	const location = useLocation();
-	const navigate = useNavigate();
-	const params = useParams();
-	return <Component {...props} router={{ location, navigate, params }} />
-};
+import { WithRouter } from "../common/withRouter/WithRouter.jsx";
 
 const mapStateToProps = (state) => ({
 	state: state.ScreensReducer,
 });
 
-export default connect(mapStateToProps, { updateAirStateThunk })(withRouter(AirProp));
+export default connect(mapStateToProps, { updateAirStateThunk })(WithRouter(AirProp));
