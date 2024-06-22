c9ecb60d7d6241ba17d18bd07ec28d7cfa9c00e4
Dmitry <dimaruzaev2@gmai.com>
2024-06-22 20:54:31 +0300

    ref

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,5 +1,5 @@
 import { App } from "./App.jsx";
 import { connect } from "react-redux";
-import { getStandardsThunk, updateAirStateThunk } from "./redux/reducers/air-data-reducer.js";
+import { updateAirStateThunk, getStandardsThunk } from "./redux/reducers/air-data-reducer.js";
 
 export default connect(null, { updateAirStateThunk, getStandardsThunk })(App);
